0890d55e5497af99d2350831d8aec6ae46429bfd
VinayJose456 <75767945+VinayJose456@users.noreply.github.com>
2021-02-24 21:12:25 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var backgroundImg;
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var database;
+var form,player,game;
+function setup(){
+  database = firebase.database();
+  
+  createCanvas(500,500);
+game = new Game();
+game.getState();
+game.start();
+
+ 
+}
+
+function draw(){
+  background("white");
+  
+    
+  
+}
+
